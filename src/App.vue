0b16a596eb6d000c4fa8373af0a9c00a30cf86b5
Diego Pertusa <mrjeffworker@MacBook-Pro-de-MRJEFF-6.local>
2022-03-08 13:56:23 +0100

    add bio

--- src/App.vue
+++ src/App.vue
@@ -1,73 +1,127 @@
 <script setup lang="ts">
-import { useAppStore } from './stores/app'
-
-const store = useAppStore()
-
+import Bio from './components/Bio.vue'
 </script>
 
 <template>
-  <Header />
-  <CountDown />
-  <Schedule />
-  <HowToArrive />
-  <Bus />
-  <Reservation />
-  <Contact />
-  <Disclaimer />
-  <Footer />
-
+  <Bio />
 </template>
 
 <style lang="postcss">
+/* ==================================== 
+   Config
+   ==================================== */
+
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@500;700&family=Roboto+Mono:wght@300&display=swap');
 @import url("./assets/reset.css");
 
 :root {
-  --primary-font: "Playfair Display", arial, serif;
-  --secondary-font: "Montserrat", baskerville, arial, sans-serif;
-  --primary-color: #3a4c3e;
-  --text-color: #706e67;
-  --background-color: #f3f2f1;
-  --error-color: #b13b3b;
+  --primary-font: "Montserrat", helvetica, arial, serif;
+  --secondary-font: 'Roboto Mono', arial, serif;
+  --primary-color: #00EECE;
+  --secondary-color: #F8A85D;
+  --dark-color: #31303A;
+  --dark-secondary-color: #28282F;
+  --dark-gray-color: #363636;
+  --gray-color: #CECED3;
+  --light-color: #F1F1F1;
+
+  --base: 8px;
+  --bold: 700;
+  --medium: 500;
+  --regular: 400;
+  --light: 300;
+
+  /* Light scheme */
+  --accent-color: var(--primary-color);
+  --background-color: var(--light-color);
+  --text-color: var(--dark-gray-color);
+  --title-color: var(--dark-color);
 }
+
+@media (prefers-color-scheme: dark) {
+  :root {
+    --accent-color: var(--primary-color);
+    --background-color: var(--dark-color);
+    --text-color: var(--gray-color);
+    --title-color: var(--light-color);
+  }
+}
+
+/* ==================================== 
+   Skeleton
+   ==================================== */
 
 body {
   background-color: var(--background-color);
-  font-family: var(--primary-font);
+  font-family: var(--secondary-font);
+  font-size: calc(var(--base) * 2.5);
+  font-weight: var(--light);
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: var(--text-color);
 }
 
+#app {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
 .section {
-  margin-top: 48px;
-  margin-bottom: 48px;
+  width: 100%;
+  margin-top: calc(var(--base) * 6);
+  margin-bottom: calc(var(--base) * 6);
 
   @media (min-width: 768px) {
-    margin-top: 80px;
-    margin-bottom: 80px;
+    margin-top: calc(var(--base) * 10);
+    margin-bottom: calc(var(--base) * 10);
   }
 
   @media (min-width: 1024px) {
-    margin-top: 104px;
-    margin-bottom: 104px;
+    margin-top: calc(var(--base) * 13);
+    margin-bottom: calc(var(--base) * 13);
   }
 }
 
 .section__wrapper {
+  width: 100%;
   max-width: 1440px;
   margin: auto;
-  padding-left: 24px;
-  padding-right: 24px;
+  padding-left: calc(var(--base) * 3);
+  padding-right: calc(var(--base) * 3);
   
 
   @media (min-width: 768px) {
-    padding-left: 32px;
-    padding-right: 32px;
+    padding-left: calc(var(--base) * 4);
+    padding-right: calc(var(--base) * 4);
   }
 
   @media (min-width: 1024px) {
-    padding-left: 2.632rem;
-    padding-right: 2.632rem;
+    padding-left: calc(var(--base) * 5);
+    padding-right: calc(var(--base) * 5);
   }
+}
+
+/* ==================================== 
+   Texts
+   ==================================== */
+
+.big-title {
+  font-family: var(--primary-font);
+  font-weight: var(--bold);
+  font-size: calc(var(--base) * 5);
+  color: var(--title-color);
+}
+
+a {
+  font-family: var(--primary-font);
+  font-weight: var(--medium);
+  color: var(--accent-color);
+  text-decoration: underline;
+}
+
+p {
+  margin-bottom: calc(var(--base) * 2);
 }
 </style>
---
+++ src/components/Bio.vue
@@ -0,0 +1,85 @@
+<script setup lang="ts">
+import Brand from '../ui/Brand.vue'
+import Github from '../ui/Github.vue'
+import Linkedin from '../ui/Linkedin.vue'
+</script>
+
+<template>
+  <section class="section">
+    <div class="bio section__wrapper">
+      
+      <img class="portrait" src="" alt="">
+      <div class="bio__column">
+
+        <Brand class="logo" />
+        <h1 class="big-title">Hi! 👋🏻 I'm Diego Pertusa Irles</h1>
+        <p>Engineering manager at Jeff</p>
+
+        <div class="link-collection">
+          <a href="" class="link-collection__item">CV</a>
+          <a href="" class="link-collection__item">About</a>
+        </div>
+
+        <div class="link-collection">
+          <Github class="link-collection__item--reduced" />
+          <Linkedin class="link-collection__item--reduced" />
+        </div>
+
+      </div>
+    </div>
+  </section>
+</template>
+
+<style lang="postcss" scoped>
+.bio {
+  @media (min-width: 768px) {
+    display: flex;
+    justify-content: center;
+  }
+}
+
+.logo {
+  margin-bottom: calc(var(--base) * 1);
+}
+
+.portrait {
+  display: block;
+  width: 200px;
+  height: 200px;
+  margin: 0 auto;
+  margin-bottom: calc(var(--base) * 4);
+  background-color: var(--title-color);
+  border-radius: 50%;
+
+  @media (min-width: 768px) {
+    margin: 0;
+  }
+}
+
+.bio__column {
+  @media (min-width: 768px) {
+    margin-left: calc(var(--base) * 2);
+  }
+
+  @media (min-width: 1024px) {
+    width: 50%;
+  }
+}
+
+.link-collection {
+  margin-bottom: calc(var(--base) * 2);
+
+  &:last-child {
+    margin-bottom: 0;
+  }
+}
+
+.link-collection__item + .link-collection__item {
+  margin-left: calc(var(--base) * 3);
+}
+
+.link-collection__item--reduced + .link-collection__item--reduced {
+  margin-left: calc(var(--base) * 2);
+}
+
+</style>
